895f06b298cc6088103b38b64041431b94f2c441
ousseynou <odieng@deloitte.fr>
2021-07-19 17:03:45 +0100

    #first developpement of utils module

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-def say_hello():
-  println("Hello World from baye fall")
+def is_prime(n):
+    numbers = range(2, int(n/2)+2)
+    stop_num = 1
+    for i in numbers:
+        stop_num = i
+        if n%i == 0:
+            break
+    if stop_num == int(n/2)+1:
+        return True
+    return False
+
+
+
